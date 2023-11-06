d687527f7431a1a435b4f02ebe8d7d9eae56b6e2
divyeshpanchasara <114856928+divyeshpanchasara@users.noreply.github.com>
2023-11-06 15:58:51 +0530

    Update MainLogic.java

--- MainLogic.java
+++ MainLogic.java
@@ -1,54 +1,27 @@
-import java.util.Scanner;
 import bank.DataBase;
 import bank.functionsOfMainLogic;
 
 public class MainLogic {
-
-    private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+        
         DataBase db = new DataBase(null, 0);
         boolean execute = true;
+        
         while (execute) {
             functionsOfMainLogic.clearScreen();
             functionsOfMainLogic.greet();
             functionsOfMainLogic.displayOptions();
             int choice = functionsOfMainLogic.myChoice();
             if(choice == 1){
-                int index =  functionsOfMainLogic.verifyUser(db);
-                if(index != -1){
-                    if( functionsOfMainLogic.pinVerification(db, index) ){
-                        db.get(index).checkBalance();
-                    }
-                    else System.out.println("Worng Pin");
-                }
-                else System.out.println("Account dosent exist!!");
+                functionsOfMainLogic.executeCheckBalance(db);
                 execute = functionsOfMainLogic.exit(execute);
             }   
             else if(choice == 2){
-                int index =  functionsOfMainLogic.verifyUser(db);
-                if(index != -1){
-                    if( functionsOfMainLogic.pinVerification(db, index) ){
-                        System.out.print("Enter Amount: ");
-                        float amount = sc.nextFloat();
-                        db.get(index).depositAmount(amount);
-                    }
-                    else System.out.println("Worng Pin");
-                }
-                else System.out.println("Account dosent exist!!");
+                functionsOfMainLogic.executeDepositeAmount(db);
                 execute = functionsOfMainLogic.exit(execute);
             }
             else if(choice == 3){
-                int index =  functionsOfMainLogic.verifyUser(db);
-                if(index != -1){
-                    if( functionsOfMainLogic.pinVerification(db, index) ){
-                        System.out.print("Enter Amount: ");
-                        float amount = sc.nextFloat();
-                        db.get(index).withdrawAmount(amount);
-                    }
-                    else System.out.println("Worng Pin");
-                }
-                else System.out.println("Account dosent exist!!");
+                functionsOfMainLogic.executeWithdrawAmount(db);
                 execute = functionsOfMainLogic.exit(execute);
             }
             else if(choice == 4){
