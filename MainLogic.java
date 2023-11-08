04cffa5882c767c95dd2b909b86a34b5cb6c544d
divyeshpanchasara <114856928+divyeshpanchasara@users.noreply.github.com>
2023-11-08 16:53:35 +0530

    Update MainLogic.java

--- MainLogic.java
+++ MainLogic.java
@@ -12,29 +12,12 @@ public class MainLogic {
             functionsOfMainLogic.greet();
             functionsOfMainLogic.displayOptions();
             int choice = functionsOfMainLogic.myChoice();
-            if(choice == 1){
-                functionsOfMainLogic.executeCheckBalance(db);
-                execute = functionsOfMainLogic.exit(execute);
-            }   
-            else if(choice == 2){
-                functionsOfMainLogic.executeDepositeAmount(db);
-                execute = functionsOfMainLogic.exit(execute);
-            }
-            else if(choice == 3){
-                functionsOfMainLogic.executeWithdrawAmount(db);
-                execute = functionsOfMainLogic.exit(execute);
-            }
-            else if(choice == 4){
-                functionsOfMainLogic.createAccount(db);
-                execute = functionsOfMainLogic.exit(execute);
-            }
-            else if(choice == 5){
-                db.showDataBase();
-                execute = functionsOfMainLogic.exit(execute);
-            }
-            else {
-                execute = false;
-            }
+            if(choice == 1) functionsOfMainLogic.executeCheckBalance(db);
+            else if(choice == 2) functionsOfMainLogic.executeDepositeAmount(db);
+            else if(choice == 3) functionsOfMainLogic.executeWithdrawAmount(db);
+            else if(choice == 4) functionsOfMainLogic.createAccount(db);
+            else if(choice == 5) db.showDataBase();
+            execute = functionsOfMainLogic.exit(execute);
         }
     }
 }
